210c5d7b8c59198ae9e87047233dabac5b0061f9
Festivalofmisfittoys <festivalofmisfittoys@gmail.com>
2026-08-27 12:48:06 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
+// September 8, 2026 Lineup Reveal
 
+const countdownDate = new Date("September 8, 2026 00:00:00").getTime();
+
+const timer = setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) /
+        (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60)) / 1000
+    );
+
+    document.getElementById("countdown").innerHTML =
+        days + "d " +
+        hours + "h " +
+        minutes + "m " +
+        seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(timer);
+
+        document.getElementById("countdown").innerHTML =
+            "LINEUP ANNOUNCED!";
+    }
+
+}, 1000);
